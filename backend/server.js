31c711e9886ea4d33cf4cbd2e0eaf26d3ca35fa2
Ahhmed Ben Mansour <ahhmedbenmansour@gmail.com>
2025-10-24 11:53:50 +0100

    ffffffffff

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT 
 
 // Middleware
 app.use(cors());
